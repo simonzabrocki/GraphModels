feae7dbb79c51f95ce35045bc14707d26f26cf08
Simon <simon.zabrocki@gmail.com>
2020-12-21 11:35:51 +0900

    update hermen

--- models/Hermen/model_1.py
+++ models/Hermen/model_1.py
@@ -1,64 +1,38 @@
-FDi_nodes = [
-    # FDTi from the previous function
-    {'type': 'input',
-     'unit': '1000 t',
-     'id': 'FDTi',
-     'name': 'Total food demand per food group',
+Animal_group = ['Bovine meat', 'Pigmeat', 'Poultry meat', 'Eggs',
+                'Milk - Excluding Butter', ('Meat, other' + 'Mutton & Goat meat')]
 
-     },
-    {'type': 'variable',
-     'unit': '1000 t',
-     'id': 'FDi_baseline',
-     'name': 'Vector total animal feed demand per crop',
-     'in': ['FMi', 'FD'],
-     'computation': {'name': 'FMi * FD',
-                     'formula': lambda X: X['FMi']*X['FD']}
-     },
-    # Note1: The vector 'FMi' might contain animal-based feed (i.e. Milk) which caused a cycle error in my previous model; so we want to select only crops from the vector FMI
-    {'type': 'variable',
-     'unit': '',
-     'id': 'FMi',
-     'name': 'Feed mix fraction',
-     'in': ['FDi'],
-     'computation': {'name': 'FDi/SUM(FDi)',
-                     'formula': lambda X: X['FDi'].loc[Crop_group]/sum(X['FDi'])}
-     },
-    {'type': 'input',
-     'unit': '1000 t',
-     'id': 'FDi',
-     'name': 'Vector feed demand',
 
-     },
-
-    # Note2: the CRfd is calculated for the year 2017; and assumed constant. For hungary the value of CRfd is: 1.58729526339088
-
-    {'type': 'variable',
-     'unit': '1ooo t',
-     'id': 'FD',
-     'name': 'Total crop-feed demand',
-     'in': ['TAFDi', 'CRfd'],
-     'computation': {'name': 'SUM(TAFDi)/CRfd',
-                     'formula': lambda X: sum(X['TAFDi'])/X['CRfd']}
-     },
-
-    # CRfd is calculated with model data from  & assumed constant for simulated years (for now)
+CRfd_nodes = [
     {'type': 'parameter',
      'unit': '',
      'id': 'CRfd',
      'name': 'feed/forage distribution (correction)',
+     'in': ['TAFDi2017_cal', 'FDi2017'],
+     'computation': {'name': 'SUM((TAFDi2017_cal/FDi2017))',
+                     'formula': lambda X: sum((X['TAFDi2017_cal'])/(X['FDi2017'].loc[Animal_group]))}
      },
-
+    {'type': 'input',
+     'unit': '1000 t',
+     'id': 'FDi2017',
+     'name': 'Vector feed demand',
+     },
+    # So this computation uses the 2017 'FPi' value to calculate the 'TAFDi2017_cal' instead of the whole food demand computation for 2017.
+    {'type': 'variable',
+     'unit': 'kg',
+     'id': 'TAFDi2017_cal',
+     'name': 'Vector total animal feed demand per animal',
+     'in': ['FCRi', 'FPi'],
+     'computation': {'name': '(FRCi*FPi)',
+                     'formula': lambda X: (X['FCRi'] * X['FPi'].loc[Animal_group])}
+     },
     {'type': 'parameter',
      'unit': 'kg DM feed/ kg EW',
      'id': 'FCRi',
      'name': 'feed conversion ratio ',
      },
-    {'type': 'variable',
-     'unit': 'kg',
-     'id': 'TAFDi',
-     'name': 'vector total animal feed demand per animal',
-     'in': ['FCRi', 'FDTi'],
-     'computation': {'name': 'FRCi*FDTi_animal',
-                     'formula': lambda X: (X['FCRi'] * X['FDTi'].loc[Animal_group])}
-     }
+    {'type': 'input',
+     'unit': '1000 t',
+     'id': 'FPi',
+     'name': 'Food production per food group',
+     },
 ]
