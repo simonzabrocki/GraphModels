da17e7faeb4e5f476830fbb4fe7f5bc02a9e9e24
Simon <simon.zabrocki@gmail.com>
2020-12-28 16:52:20 +0900

    add BE2 full model

--- models/Hermen/model_change_cropland.py
+++ models/Hermen/model_change_cropland.py
@@ -21,48 +21,48 @@ BE2_nodes = [
      },
     # for the land stocks you start with the baseline & for each iteration the old value should be stored. So for the next iteration in t+2, this updated land-stocks from t+1 are used
     # After the new land use distribution these values should be updated:
-    # IL_stock_newRF --> IL_stock
-    # FL_stock_newRF --> FL_stock
-    # CL_stock_new--> CL_stock
+    # IL_t_minus_1_newRF --> IL_t_minus_1
+    # FL_t_minus_1_newRF --> FL_t_minus_1
+    # CL_t_minus_1_new--> CL_t_minus_1
 
     {'type': 'input',
      'unit': 'x1000 ha',
-     'id': 'CL_stock',
+     'id': 'CL_t_minus_1',
      'name': 'Cropland t-1',
      },
     {'type': 'input',
      'unit': 'x1000 ha',
-     'id': 'IL_stock',
+     'id': 'IL_t_minus_1',
      'name': 'Inactive land t-1',
      },
     {'type': 'input',
      'unit': 'x1000 ha',
-     'id': 'FL_stock',
+     'id': 'FL_t_minus_1',
      'name': 'Forest land t-1',
      },
     {'type': 'variable',
-     'id': 'CL_stock_new',
+     'id': 'CL_t_minus_1_new',
      'name': 'Cropland stock',
      'unit': 'x 1000ha',
-     'in': ['chCL', 'CL_stock'],
-     'computation': {'name': 'CL_stock + chCL',
-                     'formula': lambda X: CL(X['chCL'], X['CL_stock'])}
+     'in': ['chCL', 'CL_t_minus_1'],
+     'computation': {'name': 'CL_t_minus_1 + chCL',
+                     'formula': lambda X: CL(X['chCL'], X['CL_t_minus_1'])}
      },
     {'type': 'variable',
-     'id': 'IL_stock_new',
+     'id': 'IL_t_minus_1_new',
      'name': 'Inactive land stock',
      'unit': 'x 1000 ha',
-     'in': ['chCL', 'IL_stock'],
-     'computation': {'name': 'f(chCL, CL_stock) ',
-                     'formula': lambda X: IL(X['chCL'], X['IL_stock'])}
+     'in': ['chCL', 'IL_t_minus_1'],
+     'computation': {'name': 'f(chCL, CL_t_minus_1) ',
+                     'formula': lambda X: IL(X['chCL'], X['IL_t_minus_1'])}
      },
     {'type': 'variable',
-     'id': 'FL_stock_new',
+     'id': 'FL_t_minus_1_new',
      'name': 'Forest land stock',
      'unit': 'x 1000ha',
-     'in': ['chCL', 'FL_stock', 'IL_stock_new'],
-     'computation': {'name': 'f(chCL, FL_stock, IL_stock_new)',
-                     'formula': lambda X: FL(X['chCL'], X['FL_stock'], X['IL_stock_new'])}
+     'in': ['chCL', 'FL_t_minus_1', 'IL_t_minus_1_new'],
+     'computation': {'name': 'f(chCL, FL_t_minus_1, IL_t_minus_1_new)',
+                     'formula': lambda X: FL(X['chCL'], X['FL_t_minus_1'], X['IL_t_minus_1_new'])}
      },
     {'type': 'parameter',
      'unit': '%',
@@ -73,25 +73,25 @@ BE2_nodes = [
      'id': 'RF_land',
      'name': 'Reforestation of land',
      'unit': 'x 1000ha',
-     'in': ['R_rate', 'IL_stock_new'],
-     'computation': {'name': 'R_rate*IL_stock_new',
-                     'formula': lambda X: X['R_rate']*1e-2*X['IL_stock_new']}
+     'in': ['R_rate', 'IL_t_minus_1_new'],
+     'computation': {'name': 'R_rate*IL_t_minus_1_new',
+                     'formula': lambda X: X['R_rate']*1e-2*X['IL_t_minus_1_new']}
      },
     {'type': 'variable',
-     'id': 'FL_stock_new_RF',
+     'id': 'FL_t_minus_1_new_RF',
      'name': 'New forest land stock after reforestation policy',
      'unit': 'x 1000ha',
-     'in': ['RF_land', 'FL_stock_new'],
-     'computation': {'name': 'FL_stock_new+RF_land',
-                     'formula': lambda X: X['FL_stock_new']+X['RF_land']}
+     'in': ['RF_land', 'FL_t_minus_1_new'],
+     'computation': {'name': 'FL_t_minus_1_new+RF_land',
+                     'formula': lambda X: X['FL_t_minus_1_new']+X['RF_land']}
      },
     {'type': 'variable',
-     'id': 'IL_stock_new_RF',
+     'id': 'IL_t_minus_1_new_RF',
      'name': 'New inactive land stock after reforestation policy',
      'unit': 'x 1000ha',
-     'in': ['RF_land', 'IL_stock_new'],
-     'computation': {'name': 'IL_stock_new-RF_land',
-                     'formula': lambda X: X['IL_stock_new'] - X['RF_land']}
+     'in': ['RF_land', 'IL_t_minus_1_new'],
+     'computation': {'name': 'IL_t_minus_1_new-RF_land',
+                     'formula': lambda X: X['IL_t_minus_1_new'] - X['RF_land']}
      },
     {'type': 'input',
      'unit': '1000 ha',
@@ -113,40 +113,40 @@ BE2_nodes = [
      'id': 'BE2',
      'name': 'Share of forest area to total land area',
      'unit': '%',
-     'in': ['TLA', 'FL_stock_new_RF'],
-     'computation': {'name': '100*(FL_stock_new_RF / TLA)',
-                     'formula': lambda X:  100 * (X['FL_stock_new_RF']) / X['TLA']}
+     'in': ['TLA', 'FL_t_minus_1_new_RF'],
+     'computation': {'name': '100*(FL_t_minus_1_new_RF / TLA)',
+                     'formula': lambda X:  100 * (X['FL_t_minus_1_new_RF']) / X['TLA']}
      },
     # Used in fertilizer computation -> as FAO nutrient balance uses the total cropland including permanent + fallow lands for fertilizer; LUi['land under permanent crops'] is added again; assumed constant)
     {'type': 'output',
      'id': 'TOT_CL_NEW',
      'name': 'Total new cropland (including perm. and fallow land)',
      'unit': 'x 1000ha',
-     'in': ['TCropDi_sum', 'chCL', 'LUi', 'FL_stock', 'FL_stock_new'],
-     'computation': {'name': 'TCropDi_sum+chCL+CL_perm+(IL_stock-IL_stock_new)',
-                     'formula': lambda X: (X['TCropDi_sum']-X['chCL']+X['LUi'].loc['Land under permanent crops']+(X['IL_stock']-X['IL_stock_new']))}
+     'in': ['TCropDi_sum', 'chCL', 'LUi', 'FL_t_minus_1', 'FL_t_minus_1_new'],
+     'computation': {'name': 'TCropDi_sum+chCL+CL_perm+(IL_t_minus_1-IL_t_minus_1_new)',
+                     'formula': lambda X: (X['TCropDi_sum']-X['chCL']+X['LUi'].loc['Land under permanent crops']+(X['IL_t_minus_1']-X['IL_t_minus_1_new']))}
      },
 
 
 ]
 
 
-def CL(chCL, CL_stock):
-    return chCL + CL_stock
+def CL(chCL, CL_t_minus_1):
+    return chCL + CL_t_minus_1
 
 
-def FL(chCL, FL_stock, IL_stock):
-    if (chCL > 0) and (IL_stock - chCL < 0):
-        return FL_stock + IL_stock - chCL
+def FL(chCL, FL_t_minus_1, IL_t_minus_1):
+    if (chCL > 0) and (IL_t_minus_1 - chCL < 0):
+        return FL_t_minus_1 + IL_t_minus_1 - chCL
     else:
-        return FL_stock
+        return FL_t_minus_1
 
 
-def IL(chCL, IL_stock):
-    if (chCL > 0) and (IL_stock - chCL < 0):
+def IL(chCL, IL_t_minus_1):
+    if (chCL > 0) and (IL_t_minus_1 - chCL < 0):
         return 0
     else:
-        return IL_stock - chCL
+        return IL_t_minus_1 - chCL
 
 
 model_BE2 = GraphModel(BE2_nodes)
