fdaf091bab737dc04ac867989869e5f3ae803147
Simon <simon.zabrocki@gmail.com>
2020-12-30 10:23:35 +0900

    add 2 new graphsz

--- models/Hermen/model_change_cropland.py
+++ models/Hermen/model_change_cropland.py
@@ -1,154 +1,112 @@
-# Refactored
+# These are the animal population groups that are used for the manure- and enteric emissions -> we need to aggregate to this from: ANPi & the Animal_group
+# 'Other' contains: Horses, Asses and Mules; these animal populations are quite small & generally not eaten -> so assumed constant from the 2017 dataset
 
-BE2_nodes = [
-    {'type': 'input',
-     'unit': '1000 ha',
-     'id': 'LUi',
-     'name': 'vector land use',
+other_animals = [("Asses" + "Mules" + "Horse")]
+Animal_group_emissions = ['Bovine meat', 'Pigmeat', 'Poultry meat',
+                          'Eggs', 'Milk - Excluding Butter', 'Mutton & Goat meat']
+
+
+# not sure if this works; but in this group (in ANPi_baseline) the eggs+meat should be added (in faostat ANPI doesn't have poultry meat and eggs seperated)
+ANPi_group = ["Milk - Excluding Butter", 'Bovine meat',
+              'Pigmeat', ('Poultry meat'+'Eggs'),  "Mutton & Goat meat"]
+TAHi_group = ["Milk - Excluding Butter", "Bovine meat",
+              "Pigmeat", "Poultry meat", "Mutton & Goat meat"]
+
+# Milk &Bovine are aggregated into 'cattle' ; Asses, Mules and Horse into 'other'
+Emission_groups = [("Milk - Excluding Butter" + "Bovine meat"), "Pigmeat",
+                   "Poultry meat", "Mutton & Goat meat", ("Asses" + "Mules", "Horse")]
+
+animals_nodes = [
+
+    {'type': 'variable',
+     'unit': '1000 t',
+     'id': 'FDTi',
+     'name': 'Total food demand per food group',
+
      },
 
-    # from previous part
-    {'type': 'variable',
-     'id': 'chCL',
-     'name': 'Change in cropland',
-     'unit': 'x 1000ha'
+    # add: "Sheep and Goat Meat" to AYi vector (value: 16.7) from: http://www.fao.org/faostat/en/#data/QL
+    {'type': 'input',
+     'unit': 'kg/animal',
+     'id': 'AYi',
+     'name': 'Vector of animal yields',
+
      },
 
-    {'type': 'variable',
-     'id': 'TCropDi_sum',
-     'name': 'Sum total cropland demand',
-     'unit': 'x 1000ha'
-     },
-    # for the land stocks you start with the baseline & for each iteration the old value should be stored. So for the next iteration in t+2, this updated land-stocks from t+1 are used
-    # After the new land use distribution these values should be updated:
-    # IL_t_minus_1_newRF --> IL_t_minus_1
-    # FL_t_minus_1_newRF --> FL_t_minus_1
-    # CL_t_minus_1_new--> CL_t_minus_1
 
-    {'type': 'input',
-     'unit': 'x1000 ha',
-     'id': 'CL_t_minus_1',
-     'name': 'Cropland t-1',
-     },
-    {'type': 'input',
-     'unit': 'x1000 ha',
-     'id': 'IL_t_minus_1',
-     'name': 'Inactive land t-1',
-     },
-    {'type': 'input',
-     'unit': 'x1000 ha',
-     'id': 'FL_t_minus_1',
-     'name': 'Forest land t-1',
-     },
     {'type': 'variable',
-     'id': 'CL_t_minus_1_new',
-     'name': 'Cropland stock',
-     'unit': 'x 1000ha',
-     'in': ['chCL', 'CL_t_minus_1'],
-     'computation': {'name': 'CL_t_minus_1 + chCL',
-                     'formula': lambda X: CL(X['chCL'], X['CL_t_minus_1'])}
+     'unit': 'x1 million animals',
+     'id': 'ANPi',
+     'name': 'Vector animals needed for production',
+     'in': ['FDTi', 'AYi'],
+     'computation': {'name': 'FDTi/AYi',
+                     'formula': lambda X: X['FDTi'].loc[Animal_group_emissions] / X['AYi'].loc[Animal_group_emissions]
+                     }
+
      },
+
+
+    # ANPI data from 2017 is used to calculate the PTTAi -> in JSON file this is still 'ANPi'
+    # ADD: "Sheep and Goats" to JSON file for ANPi_baseline, value: 107764
+    {'type': 'input',
+     'unit': 'heads',
+     'id': 'ANPi_baseline',
+     'name': 'Vector total production animals (2017)',
+     },
+
+
+    # ADD: "Sheep and Goats" to JSON file for TAHi_baseline, value: 1222000
+    {'type': 'input',
+     'unit': 'heads',
+     'id': 'TAHi_baseline',
+     'name': 'Vector total animals (2017)',
+     },
+
+
     {'type': 'variable',
-     'id': 'IL_t_minus_1_new',
-     'name': 'Inactive land stock',
-     'unit': 'x 1000 ha',
-     'in': ['chCL', 'IL_t_minus_1'],
-     'computation': {'name': 'f(chCL, CL_t_minus_1) ',
-                     'formula': lambda X: IL(X['chCL'], X['IL_t_minus_1'])}
+     'unit': '',
+     'id': 'PTTAi',
+     'name': 'vector production-to-total animals ratio',
+     'in': ['TAHi_baseline', 'ANPi_baseline'],
+     'computation': {'name': 'TAHi_baseline/ANPi_baseline',
+                     'formula': lambda X: (X['TAHi_baseline'].loc[TAHi_group] / X['ANPi_baseline'].loc[ANPi_group])}
      },
+
+
     {'type': 'variable',
-     'id': 'FL_t_minus_1_new',
-     'name': 'Forest land stock',
-     'unit': 'x 1000ha',
-     'in': ['chCL', 'FL_t_minus_1', 'IL_t_minus_1_new'],
-     'computation': {'name': 'f(chCL, FL_t_minus_1, IL_t_minus_1_new)',
-                     'formula': lambda X: FL(X['chCL'], X['FL_t_minus_1'], X['IL_t_minus_1_new'])}
+     'unit': 'heads',
+     'id': 'TAPi',
+     'name': 'Vector total animal population (heads)',
+     'in': ['ANPi', 'PTTAi'],
+     'computation': {'name': 'ANPi*PTTAi',
+                     'formula': lambda X: np.array([(X['ANPi'].loc[ANPi_group] * X['PTTAi'].loc[TAHi_group]), sum(X['TAHi_baseline'].loc[other_animals])])}
      },
+
+
+
+    # the data on FAOstat did not match 100% so a small correction was used (still need to check why this difference emerges)  --> calculated using 2017 input data & comparing it with FAO data
     {'type': 'parameter',
-     'unit': '%',
-     'id': 'R_rate',
-     'name': 'Rate of reforestation',
+     'unit': '',
+     'id': 'CPTAi_cal',
+     'name': 'correction total animals (vector)',
      },
-    {'type': 'variable',
-     'id': 'RF_land',
-     'name': 'Reforestation of land',
-     'unit': 'x 1000ha',
-     'in': ['R_rate', 'IL_t_minus_1_new'],
-     'computation': {'name': 'R_rate*IL_t_minus_1_new',
-                     'formula': lambda X: X['R_rate']*1e-2*X['IL_t_minus_1_new']}
+
+
+    {'type': 'parameter',
+     'unit': 'heads',
+     'id': 'TAi',
+     'name': 'Vector total animal population corrected (heads)',
+     'in': ['CPTAi_cal', 'TAPi'],
+     'computation': {'name': 'TAPi*CPTAi_cal',
+                     'formula': lambda X: X['TAPi'] * X['CPTAi_cal']}
      },
-    {'type': 'variable',
-     'id': 'FL_t_minus_1_new_RF',
-     'name': 'New forest land stock after reforestation policy',
-     'unit': 'x 1000ha',
-     'in': ['RF_land', 'FL_t_minus_1_new'],
-     'computation': {'name': 'FL_t_minus_1_new+RF_land',
-                     'formula': lambda X: X['FL_t_minus_1_new']+X['RF_land']}
-     },
-    {'type': 'variable',
-     'id': 'IL_t_minus_1_new_RF',
-     'name': 'New inactive land stock after reforestation policy',
-     'unit': 'x 1000ha',
-     'in': ['RF_land', 'IL_t_minus_1_new'],
-     'computation': {'name': 'IL_t_minus_1_new-RF_land',
-                     'formula': lambda X: X['IL_t_minus_1_new'] - X['RF_land']}
-     },
-    {'type': 'input',
-     'unit': '1000 ha',
-     'id': 'TLA',
-     'name': 'Total land area',
-     'in': ['LUi'],
-     'computation': {'name': 'Select',
-                     'formula': lambda X: X['LUi'].loc['Land area']}
-     },
-    {'type': 'input',
-     'unit': '1000 ha',
-     'id': 'TFA2017',
-     'name': 'Forest land area (2017)',
-     'in': ['LUi'],
-     'computation': {'name': 'Select',
-                     'formula': lambda X: X['LUi'].loc['Forest land']}
-     },
-    {'type': 'output',
-     'id': 'BE2',
-     'name': 'Share of forest area to total land area',
-     'unit': '%',
-     'in': ['TLA', 'FL_t_minus_1_new_RF'],
-     'computation': {'name': '100*(FL_t_minus_1_new_RF / TLA)',
-                     'formula': lambda X:  100 * (X['FL_t_minus_1_new_RF']) / X['TLA']}
-     },
-    # Used in fertilizer computation -> as FAO nutrient balance uses the total cropland including permanent + fallow lands for fertilizer; LUi['land under permanent crops'] is added again; assumed constant)
-    {'type': 'output',
-     'id': 'TOT_CL_NEW',
-     'name': 'Total new cropland (including perm. and fallow land)',
-     'unit': 'x 1000ha',
-     'in': ['TCropDi_sum', 'chCL', 'LUi', 'FL_t_minus_1', 'FL_t_minus_1_new'],
-     'computation': {'name': 'TCropDi_sum+chCL+CL_perm+(IL_t_minus_1-IL_t_minus_1_new)',
-                     'formula': lambda X: (X['TCropDi_sum']-X['chCL']+X['LUi'].loc['Land under permanent crops']+(X['IL_t_minus_1']-X['IL_t_minus_1_new']))}
-     },
+
+
+
 
 
 ]
 
+G7 = GraphModel(animals_nodes)
 
-def CL(chCL, CL_t_minus_1):
-    return chCL + CL_t_minus_1
-
-
-def FL(chCL, FL_t_minus_1, IL_t_minus_1):
-    if (chCL > 0) and (IL_t_minus_1 - chCL < 0):
-        return FL_t_minus_1 + IL_t_minus_1 - chCL
-    else:
-        return FL_t_minus_1
-
-
-def IL(chCL, IL_t_minus_1):
-    if (chCL > 0) and (IL_t_minus_1 - chCL < 0):
-        return 0
-    else:
-        return IL_t_minus_1 - chCL
-
-
-model_BE2 = GraphModel(BE2_nodes)
-
-model_BE2.draw()
+G7.draw(draw_properties)
