b8a56314ebae2f2eb5d9b997d552c72e88ef2e02
Simon <simon.zabrocki@gmail.com>
2021-04-26 11:05:51 +0900

    format for packaging

---
+++ ggmodel/__init__.py
@@ -0,0 +1,3 @@
+from ggmodel.graphmodel import (
+    GraphModel
+    )
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    required = [r.split('==')[0] for r in required]
+
+setup(
+    name='ggmodel',
+    version='0.0.1',
+    description='A Python package for creating graphical models',
+    url='https://github.com/Global-Green-Growth-Institute/GraphModels',
+    author='Simon Zabrocki',
+    author_email='simon.zabrocki@gmail.com',
+    license='BSD 2-clause',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['ggmodel'],
+    package_dir={'ggmodel': 'graphmodels/'},
+    install_requires=required,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
